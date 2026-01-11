c25f1d3d807db54020860806c5994b856de702b5
Haochen Yang <leoyang_101@outlook.com>
2026-01-12 01:00:41 +0800

    gpu

--- down.sh
+++ down.sh
@@ -1,7 +1,8 @@
 docker compose -f traefik/docker-compose.yml down
 docker compose -f apps/docker-compose.yml down
 docker compose -f cloudflare/docker-compose.yml down
-
+docker compose -f observability/docker-compose.yml down
+docker compose -f ai/docker-compose.yml down
 
 
 
